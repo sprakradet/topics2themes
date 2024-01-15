8503febccd114adef1aea40c5863a1cf317bc3d5
mariask2 <maria.skeppstedt@abm.uu.se>
2024-01-15 14:59:02 +0100

    Use topic label as name of folder

--- export_to_wordrain_format.py
+++ export_to_wordrain_format.py
@@ -1,21 +1,5 @@
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-import json
-import matplotlib.markers as markers
-from matplotlib.ticker import (MultipleLocator, AutoMinorLocator, MaxNLocator, FixedLocator)
-from matplotlib import cm
-import math
-import matplotlib.colors as colors
 import os
-import matplotlib.dates as mdates
-from math import modf
-import sys
-import datetime
-import matplotlib
-from collections import Counter
-
+import json
 
 ###
 # Start
@@ -28,17 +12,20 @@ def create_wordrain_dir(model_file, outputdir, new_dir_name, top_nr_documents = 
     if os.path.exists(new_dir):
         print(new_dir, "Already exists")
         exit()
-    
     os.mkdir(new_dir)
-    
-    
+
     with open(model_file, 'r') as f:
         data = f.read()
         obj = json.loads(data)
 
 
-    documents_for_topic = {}
+    topic_id_label_dict = {}
+    topics = obj["topic_model_output"]["topics"]
+    for topic in topics:
+        topic_id_label_dict[topic["id"]] = topic["label"]
+
     
+    documents_for_topic = {}
     for el in obj["topic_model_output"]["documents"]:
         base_name = el["base_name"]
         document_text = el["text"]
@@ -52,13 +39,15 @@ def create_wordrain_dir(model_file, outputdir, new_dir_name, top_nr_documents = 
             documents_for_topic[t["topic_index"]].append((t["topic_confidence"], document_text))
         
     for index in sorted(documents_for_topic.keys()):
-        dir_to_store = os.path.join(new_dir, str(index))
+        topic_label = topic_id_label_dict[index].replace(" ", "").replace('"', '')
+        dir_to_store = os.path.join(new_dir, topic_label)
         os.mkdir(dir_to_store)
-        for nr, (conf, text) in enumerate(documents_for_topic[index]):
+        sorted_documents = sorted(documents_for_topic[index], reverse=True)
+        top_documents = sorted_documents[:top_nr_documents]
+        for nr, (conf, text) in enumerate(top_documents):
             file_name = os.path.join(dir_to_store, str(nr) + ".txt")
             with open(file_name, "w") as out:
                 out.write(text)
     
 
-
-create_wordrain_dir("/Users/marsk757/journalpolls/code_and_data/t2tdata/data_folder/polls/topics2themes_exports_folder_created_by_system/65a4e258f52ff00acae28c0f_model.json", "/Users/marsk757/journalpolls/code_and_data/wordrain_output", "test")
+create_wordrain_dir("/Users/marsk757/journalpolls/code_and_data/t2tdata/data_folder/polls/topics2themes_exports_folder_created_by_system/65a4e258f52ff00acae28c0f_model.json", "/Users/marsk757/journalpolls/code_and_data/wordrain_output", "wordrain-polls", top_nr_documents=100)
